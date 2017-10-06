a07466dfac7181ee356443edfd075ad1ee1c2a3c
Jan Dobes <jdobes@redhat.com>
2017-10-06 16:26:21 +0200

    remove threading part as it doesn't really work anyway

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import configparser
 import logging
 import socket
-from threading import Thread
+import time
 
 
 config = configparser.ConfigParser()
@@ -17,14 +17,13 @@ else:
 
 logging.basicConfig(filename='./ircghost.log', level=level)
 
-user_aliases = {}
-user_threads = {}
-alias_to_user_channel = {}
+karma_requests = {}
 
 
 def connect(nick):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((config['server'], int(config['port'])))
+    sock.settimeout(int(config['socket_timeout']))
     sock.send(bytes("USER " + nick + " " + nick + " " + nick + " " + nick + "\r\n", "UTF-8"))
     sock.send(bytes("NICK " + nick + "\r\n", "UTF-8"))
     return sock
@@ -54,53 +53,18 @@ def finish(sock):
     sock.close()
 
 
-class IrcThread(Thread):
-    def __init__(self, nick, messages):
-        Thread.__init__(self)
-        self.nick = nick
-        self.sock = connect(nick)
-        self.sock.settimeout(5)
-        joinchan(self.sock, config['home_channel'])
-        self.messages = messages
-
-    def run(self):
-        for message in self.messages:
-            sendmsg(self.sock, message, config['ask'])
-        while 1:
-            try:
-                ircmsg = self.sock.recv(2048).decode("UTF-8")
-            except socket.timeout:
-                logging.info("Nothing received, shutting thread down.")
-                break
-            ircmsg = ircmsg.strip('\n\r')
-            parts = ircmsg.split()
-            if parts[1] == "PRIVMSG":
-                name_from = ircmsg.split('!', 1)[0][1:]
-                name_to = ircmsg.split('PRIVMSG ', 1)[1].split(' :', 1)[0]
-                message = ircmsg.split('PRIVMSG', 1)[1].split(':', 1)[1]
-                # Response from karma bot
-                if name_from == config['ask'] and name_to == self.nick:
-                    sendmsg(self.sock, message, config['botnick'])
-            elif parts[0] == "PING":
-                pong(self.sock, parts[1])
-        finish(self.sock)
-
-
-def thread_send(sock, name_from, name_to, msgs):
-    if name_from not in user_threads or not user_threads[name_from].isAlive():
-        if name_from not in user_aliases:
-            new_user = config['karma_user_prefix'] + str(len(user_aliases))
-            user_aliases[name_from] = new_user
-        else:
-            new_user = user_aliases[name_from]
-        # Save reference between user alias and original channel/user where the message was catched
-        alias_to_user_channel[new_user] = name_to
-        new_thread = IrcThread(new_user, msgs)
-        new_thread.setDaemon(True)
-        new_thread.start()
-        user_threads[name_from] = new_thread
+def where_to_send_response(channels, name_from, name_to):
+    # Check if the request message was send to channel or directly to bot
+    if name_to in channels or name_to == config['home_channel']:
+        return name_to
     else:
-        logging.info("User thread is running, skipping.")
+        return name_from
+
+
+def register_karma_request(word, respond_to):
+    if word not in karma_requests:
+        karma_requests[word] = set()
+    karma_requests[word].add((time.time(), respond_to))
 
 
 def main():
@@ -111,30 +75,68 @@ def main():
         joinchan(bot, channel)
     try:
         while 1:
-            ircmsg = bot.recv(2048).decode("UTF-8")
-            ircmsg = ircmsg.strip('\n\r')
-            parts = ircmsg.split()
-            if parts[1] == "PRIVMSG":
-                name_from = ircmsg.split('!', 1)[0][1:]
-                name_to = ircmsg.split('PRIVMSG ', 1)[1].split(' :', 1)[0]
-                message = ircmsg.split('PRIVMSG', 1)[1].split(':', 1)[1]
-                words = message.split()
-                # Repeat response from child threads to original channel/user
-                if name_from in user_aliases.values():
-                    sendmsg(bot, message, alias_to_user_channel[name_from])
-                    alias_to_user_channel[name_from] = ""
-                # Regular chat message, parse if it's sane
-                elif name_from != config['ask'] and (name_to in channels or name_to == config['home_channel']):
-                    filtered_msgs = []
-                    for i, word in enumerate(words):
-                        if word in ("rank", "srank") and i < (len(words) - 1):
-                            filtered_msgs.append(word + " " + words[i + 1])
-                        elif word.endswith("++") or word.endswith("--"):
-                            filtered_msgs.append(word)
-                    if filtered_msgs:
-                        thread_send(bot, name_from, name_to, filtered_msgs)
-            elif parts[0] == "PING":
-                    pong(bot, parts[1])
+            try:
+                ircmsg = bot.recv(2048).decode("UTF-8")
+            except socket.timeout:
+                ircmsg = None
+            # Something received
+            if ircmsg:
+                ircmsg = ircmsg.strip('\n\r')
+                parts = ircmsg.split()
+                if parts[1] == "PRIVMSG":
+                    name_from = ircmsg.split('!', 1)[0][1:]
+                    name_to = ircmsg.split('PRIVMSG ', 1)[1].split(' :', 1)[0]
+                    message = ircmsg.split('PRIVMSG', 1)[1].split(':', 1)[1]
+                    words = message.split()
+                    # Repeat response from contact to original channel
+                    if name_from == config['ask'] and name_to == config['botnick']:
+                        karma_keyword = None
+                        # rank, ++/--
+                        if len(words) > 3 and words[1] == "has" or words[2] == "has":
+                            karma_keyword = words[0]
+                        # need to wait
+                        elif len(words) > 14 and words[4] == "wait":
+                            karma_keyword = words[13][:-2]
+
+                        if karma_keyword:
+                            if karma_keyword in karma_requests:
+                                respond_to = karma_requests[karma_keyword]
+                                del karma_requests[karma_keyword]
+                                for target in respond_to:
+                                    sendmsg(bot, message, target[1])
+                            else:
+                                # WTF
+                                sendmsg(bot, "Received unexpected karma message:", config['home_channel'])
+                                sendmsg(bot, message, config['home_channel'])
+                        else:
+                            sendmsg(bot, message, config['home_channel'])
+                    # Regular chat message, parse if it's sane
+                    elif name_to in channels or name_to == config['home_channel'] or name_to == config['botnick']:
+                        respond_to = where_to_send_response(channels, name_from, name_to)
+                        for i, word in enumerate(words):
+                            if word == "rank" and i + 1 <= (len(words) - 1):
+                                register_karma_request(words[i + 1], respond_to)
+                                sendmsg(bot, "%s %s" % (word, words[i + 1]), config['ask'])
+                            elif word.endswith("++") or word.endswith("--"):
+                                register_karma_request(word[:-2], respond_to)
+                                sendmsg(bot, word, config['ask'])
+                elif parts[0] == "PING":
+                        pong(bot, parts[1])
+            else:
+                # Cleanup old requests
+                empty_keys = []
+                for key, targets in karma_requests.items():
+                    to_delete = []
+                    for target in targets:
+                        if time.time() > target[0] + int(config['cleanup_timeout']):
+                            to_delete.append(target)
+                    for item in to_delete:
+                        targets.remove(item)
+                    if not targets:
+                        empty_keys.append(key)
+                for key in empty_keys:
+                    del karma_requests[key]
+
     except KeyboardInterrupt:
         finish(bot)
 
